7f8c590e01b340a2fc61979c19ab87ad7e814ba6
Alejandro Exequiel Hernández Lara <Alejandrehl@icloud.com>
2020-01-20 18:43:12 -0300

    Socket io configurado en el backend

--- server/server.js
+++ server/server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const path = require('path');
+const socketIO = require("socket.io")
+const http = require("http")
+
 const app = express();
+let server = http.createServer(app)
 
 const publicPath = path.resolve(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
+let io = socketIO(server)
 
-app.listen(port, (err) => {
+server.listen(port, (err) => {
     if (err) throw new Error(err);
     console.log(`Server running port ${ port }`);
 });
